6b7fb9fbe5e7651795c5c41da71bea8a7c74a56a
martingalefan <yya234@g.uky.edu>
2016-12-01 01:16:52 -0500

    Update RcppExport.R

--- R/RcppExport.R
+++ R/RcppExport.R
@@ -2,6 +2,12 @@ lambdaoo<-function(kmctime,delta,lambda,gtmat){
 .Call('kmcomegalambda', PACKAGE = 'kmc',kmctime,delta,lambda,gtmat)
 }
 
+check_G_mat <- function(gmat){
+.Call('kmcRCPP_RevCHECK',PACKAGE='kmc2',gmat)
+}
+
+
+
 kmcdata_rcpp<-function(kmctime,delta,lambda,gtmat){
     .Call('kmcRCPP_KMCDATA', PACKAGE = 'kmc',kmctime,delta,lambda,gtmat)
 }
